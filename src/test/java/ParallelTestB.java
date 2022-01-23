3fbe5ca81b7af340bd3fd09a1d257d788770345f
akinkayagithub <akinkaya.b@gmail.com>
2022-01-23 09:54:40 -0600

    added parallel testing

---
+++ src/test/java/ParallelTestB.java
@@ -0,0 +1,24 @@
+import org.testng.annotations.Test;
+
+public class ParallelTestB {
+    @Test()
+    public void method1() throws InterruptedException {
+        Thread.sleep(11000);
+        System.out.println("This is method1 from class B");
+        System.out.println(Thread.currentThread().getId() + "\n");
+    }
+
+    @Test()
+    public void method2() throws InterruptedException {
+        Thread.sleep(12000);
+        System.out.println("This is method2 from class B");
+        System.out.println(Thread.currentThread().getId() + "\n");
+    }
+
+    @Test()
+    public void method3() throws InterruptedException {
+        Thread.sleep(13000);
+        System.out.println("This is method3 from class B");
+        System.out.println(Thread.currentThread().getId() + "\n");
+    }
+}
