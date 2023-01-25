0bc8b63930e71b3b191a3de98d94d9dc3c89914b
akinkayagithub <akinkaya.b@gmail.com>
2023-01-25 15:50:16 -0600

    added DataProvider documentation

--- src/test/java/_06_DataProviderTest.java
+++ src/test/java/_06_DataProviderTest.java
@@ -2,22 +2,48 @@ import org.testng.annotations.Test;
 import org.testng.annotations.DataProvider;
 
 public class _06_DataProviderTest {
+    /*
+    TestNG comes up with DataProvider to automate the process of providing test cases for execution.
+    DataProvider helps with data-driven test cases that carry the same methods but can be run multiple times with different data sets.
+    It also helps in providing complex parameters to the test methods.
+
+    You can see 2 separate tests below utilizing different data sets to execute the same test repeatedly with different data.
+
+    The result of signIn test will be as below
+        Username =
+        Password =
+        Message = Invalid Username entered!
+        Username = john
+        Password = Test1234
+        Message = Invalid Username entered!
+        Username = john
+        Password = abcd
+        Message = Invalid Username entered!
+        Username = TechGlobal
+        Password = abcd
+        Message = Invalid Password entered!
+
+
+     The result of search test will be as below
+        Company = Apple
+        Company = Tesla
+        Company = Microsoft
+     */
 
     @Test(dataProvider = "loginCredentials")
-    public void signIn(String username, String password, String message){
-        System.out.println("\n--------------------------\n");
+    public void signIn(String username, String password, String message) {
         System.out.println("Username = " + username);
         System.out.println("Password = " + password);
         System.out.println("Message = " + message);
     }
 
     @Test(dataProvider = "search")
-    public void search(String company){
+    public void search(String company) {
         System.out.println("Company = " + company);
     }
 
-    @DataProvider
-    public Object[] loginCredentials(){
+    @DataProvider(name = "loginCredentials")
+    public Object[] loginCredentials() {
         return new Object[][]{
                 {"", "", "Invalid Username entered!"},
                 {"john", "Test1234", "Invalid Username entered!"},
@@ -27,8 +53,8 @@ public class _06_DataProviderTest {
     }
 
 
-    @DataProvider
-    public Object[] search(){
+    @DataProvider(name = "search")
+    public Object[] search() {
         return new Object[]{"Apple", "Tesla", "Microsoft"};
     }
 }
